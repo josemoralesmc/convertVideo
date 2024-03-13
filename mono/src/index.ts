bfac3001666c2db1c8d54b760fe73c8ac8afc99d
josemoralesmc <josemorales270102@gmail.com>
2024-03-13 20:47:20 -0300

    aws

--- mono/src/index.ts
+++ mono/src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://convertvideo-1.onrender.com',
   credentials: true,
 }))
 app.use(morgan('dev'))
